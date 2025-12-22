08ee962e47766d78588556e8149ba643ce746e99
Rishabh Bajpai <rishabhbajpai24@gmail.com>
2025-12-21 23:24:32 -0600

    feat: Allow app port to be configured via environment variable.

--- run.py
+++ run.py
@@ -2,5 +2,7 @@ from app import create_app
 
 app = create_app()
 
+import os
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5011)
+    port = int(os.getenv('PORT', 5011))
+    app.run(debug=True, host='0.0.0.0', port=port)
